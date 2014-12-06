5cbf40b5abef7a6360ca4464b052830d54ac3f7f
MatheusXtz <matheusxtz96@gmail.com>
2014-12-06 16:37:32 -0200

    update class

--- user/app/controllers/UserCrud.java
+++ user/app/controllers/UserCrud.java
@@ -3,27 +3,27 @@ package controllers;
 
 import java.util.List;
 
-import models.User;
+import models.Usuario;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class UserCrud extends Controller {
 
-	private static final Form<User> userForm = Form.form(User.class);
+	private static final Form<Usuario> userForm = Form.form(Usuario.class);
 
 	public static Result listarUser() {
-		List<User> users = User.find.findList();
+		List<Usuario> users = Usuario.find.findList();
 		return ok(views.html.curso.render(users));
 	}
 	public static Result gravar() {
-		Form<User> form = userForm.bindFromRequest();
+		Form<Usuario> form = userForm.bindFromRequest();
 
 		if (form.hasErrors()) {
 			flash("erro", "Foram identificados erros no cadastro!");
 			return redirect(routes.UserCrud.listarUser());
 		}
-		User user = form.get();
+		Usuario user = form.get();
 		user.save();
 
 		flash("sucesso", "Dados Gravados com sucesso");
@@ -31,14 +31,14 @@ public class UserCrud extends Controller {
 	}
 	
 		public static Result detalhar(Long id) {
-			Form<User> userForm = Form.form(User.class).fill(User.find.byId(id));
+			Form<Usuario> userForm = Form.form(Usuario.class).fill(Usuario.find.byId(id));
 			return ok(views.html.alterarCurso.render(id, userForm));
 		}
 
 		public static Result alterar(Long id) {
-			Form.form(User.class).fill(User.find.byId(id));
+			Form.form(Usuario.class).fill(Usuario.find.byId(id));
 
-			Form<User> alterarForm = Form.form(User.class).bindFromRequest();
+			Form<Usuario> alterarForm = Form.form(Usuario.class).bindFromRequest();
 			if (alterarForm.hasErrors()) {
 				return badRequest(views.html.alterarCurso.render(id, alterarForm));
 			}
@@ -50,7 +50,7 @@ public class UserCrud extends Controller {
 		}
 
 		public static Result remover(Long id) {
-			User.find.ref(id).delete();
+			Usuario.find.ref(id).delete();
 			flash("sucesso", "Curso removido com sucesso");
 			return listarUser();
 		}
--- user/app/models/Usuario.java
+++ user/app/models/Usuario.java
@@ -7,15 +7,15 @@ import javax.persistence.Id;
 import play.db.ebean.Model;
 
 @Entity
-public class User extends Model {
+public class Usuario extends Model {
     @Id
     @GeneratedValue
 	private Long idUser;
     
     private String nome;
     
-    public static Model.Finder<Long, User> find = new Model.Finder<Long, User>(
-			Long.class, User.class);
+    public static Model.Finder<Long, Usuario> find = new Model.Finder<Long, Usuario>(
+			Long.class, Usuario.class);
 
 	public Long getIdUser() {
 		return idUser;
